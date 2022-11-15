444e7c3c412ad73b0a5bd1d3ea4327eb9504a097
Etienne Rwakineza <etienne.rwakineza@gmail.com>
2022-11-15 09:16:24 -0500

    AD ps Script to Extract OU Location
    AD ps Script to Extract OU Location

---
+++ ExportADworkstation_CN.ps1
@@ -0,0 +1,5 @@
+﻿#Export only Canonical and CN
+  Get-Content C:\bObbUs\tmp\AD\server_list.txt |
+  Get-ADComputer -Properties * |
+  
+  Select-Object CanonicalName, CN | Export-CSV "C:\bObbUs\tmp\AD\ADComputersCN4.CSV" -NoTypeInformation -Encoding UTF8
